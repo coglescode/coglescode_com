8b3bd043cb2ea8b33ded359d3107a1a440c8cac9
Leonard Cogles Bonne <19336315+coglescode@users.noreply.github.com>
2022-02-16 15:05:40 +0100

    Update settings.py

--- coglescode_com/settings.py
+++ coglescode_com/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 import json
 
-with open('/etc/config.json') as config_file:
+with open('/home/coglescode/coglescode_com/config.json') as config_file:
         config = json.load(config_file)
 
 
