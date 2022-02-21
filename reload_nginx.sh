36af02105b6c79feb4d888b0f2a4156b6acb1538
Leonard Cogles Bonne <19336315+coglescode@users.noreply.github.com>
2022-02-21 13:29:41 +0100

    Update reload_nginx.sh

--- reload_nginx.sh
+++ reload_nginx.sh
@@ -1,1 +1,4 @@
+pip install -r /home/coglescode/coglescode_com/requirements.txt
+python manage.py collectstatic --noinput
 sudo systemctl restart nginx.service
+sudo systemctl restart emperor.uwsgi.service
