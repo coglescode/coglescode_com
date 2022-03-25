2f661482a3eca54a0bf476e5691e0ad32beb17cc
coglescode <cogles_bonne@yahoo.com>
2022-03-25 20:37:46 +0100

    blog app urlspatterns added

--- coglescode_com/urls.py
+++ coglescode_com/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
+    path('blog/', include('blog.urls', namespace='blog')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
