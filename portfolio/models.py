2f8175c99894b100b9d8c43ad634caf6e81e2f6d
coglescode <cogles_bonne@yahoo.com>
2022-04-16 09:48:56 +0200

    Message field in Contact class changed to Charfield

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,17 +3,18 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 # Create your models here.
 
 
 class UserIntro(models.Model):
     name = models.CharField(max_length=200)
-    #name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_names')
+    # name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_names')
     intro_line = models.CharField(max_length=200)
-    descr = models.TextField()                       # descr: abbreviation of the word description
+    descr = models.TextField()  # descr: abbreviation of the word description
     avatar = models.ImageField(upload_to='images')
-    cert = models.FileField(upload_to='images')      # cert: abbreviation of the word certificate
-   
+    cert = models.FileField(upload_to='images')  # cert: abbreviation of the word certificate
+
     def __str__(self):
         return f'{self.name}'
 
@@ -35,9 +36,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=80)
     email = models.EmailField()
     subject = models.CharField(max_length=100)
-    #msg = models.TextField()
+    # msg = models.TextField()
     msg = models.CharField(max_length=80)
-
 
     def __str__(self):
         return self.name
