c8155200d96cf625ca71221316da56ad811b3f33
coglescode <cogles_bonne@yahoo.com>
2022-04-20 22:25:54 +0200

    SITEMAP added

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 from . import views
-#from blog.models import Post
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import PostSitemap
 
-app_name  = 'blog'
+# from blog.models import Post
+
+app_name = 'blog'
+
+sitemaps = {
+    # 'static': StaticViewSitemap,
+    'post': PostSitemap,
+}
 
 urlpatterns = [
-  path('', views.post_list, name='post_list'),
-  path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
-  
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+
 ]
