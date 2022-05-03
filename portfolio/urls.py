92d9e08a77dc25139c54ccc988b948d0af0a82e2
coglescode <cogles_bonne@yahoo.com>
2022-05-03 15:42:20 +0200

    Some changes made

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,10 +1,13 @@
+from django.contrib import sitemaps
 from django.urls import path
 from . import views
-#from portfolio.views import IndexView
+from django.contrib.sitemaps.views import sitemap
+# from portfolio.views import IndexView
 
-#app_name  = 'portfolio'
+# app_name  = 'portfolio'
 
 urlpatterns = [
   path('', views.IndexView.as_view(), name='index'),
   path('aboutme/', views.AboutView.as_view(), name='aboutme'),
+  # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
